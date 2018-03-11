0b48b860c645fd32296dc39d9bda4bdaf3af8866
yadong.zhu@gmail.com <yadong.zhu@gmail.com>
2018-03-12 01:26:05 +0800

    modify package,add server js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+ 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+ 
+ 
+// default route
+app.get('/', function (req, res) {
+    return res.send({ error: true, message: 'hello, Biosensors bxapi!' })
+});
+ 
+// port must be set to 8080 because incoming http requests are routed from port 80 to port 8080
+app.listen(8080, function () {
+    console.log('Node app is running on port 8080');
+});
