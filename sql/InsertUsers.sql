859cb4f5594c4f052306c87d68e1a3c93cfe2025
yadong.zhu@gmail.com <yd.zhu@bitsg.bsi.corp>
2018-05-24 10:27:55 +0800

    add TO item detail

---
+++ sql/InsertUsers.sql
@@ -0,0 +1,3 @@
+insert into dbo.BX_UserProfile values ('sz.chen','Z01','BITSG','normal',1)
+insert into dbo.BX_UserProfile values ('tw.lee','Z01','BITSG','normal',1)
+insert into dbo.BX_UserProfile values ('ch.yeo','Z01','BITSG','normal',1)
