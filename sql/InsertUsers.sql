effaaf622985a18f5c34726a7d47d9384f5de5cc
Yadong Zhu <yadong.zhu@gmail.com>
2018-07-24 17:57:12 +0800

    clean up unused files

--- sql/InsertUsers.sql
+++
@@ -1,3 +0,0 @@
-insert into dbo.BX_UserProfile values ('sz.chen','Z01','BITSG','normal',1)
-insert into dbo.BX_UserProfile values ('tw.lee','Z01','BITSG','normal',1)
-insert into dbo.BX_UserProfile values ('ch.yeo','Z01','BITSG','normal',1)
